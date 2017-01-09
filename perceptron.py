d5c29e6e0311b76fa9ec439f09a2deb091997f36
ujiro99 <siro.cola@gmail.com>
2017-01-09 13:52:59 +0900

    2.5.2

--- perceptron.py
+++ perceptron.py
@@ -32,3 +32,16 @@ def OR(x1, x2):
         return 0
     else:
         return 1
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
