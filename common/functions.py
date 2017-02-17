6e384754cb08f20d468ab7134196f9de5d8b712b
ujiro99 <siro.cola@gmail.com>
2017-02-18 04:02:59 +0900

    4.5.1

--- common/functions.py
+++ common/functions.py
@@ -1,12 +1,19 @@
 import numpy as np
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
 
-    return y
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
 
 
 def cross_entropy_error(y, t):
