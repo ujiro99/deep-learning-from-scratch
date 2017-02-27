72b9b182e27a0353fd4abb7d5c6c3b57cb8b4d57
ujiro99 <siro.cola@gmail.com>
2017-02-28 01:35:37 +0900

    5.6.3

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from common.functions import *
 
 
 class Relu:
@@ -48,4 +49,22 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
         return dx
